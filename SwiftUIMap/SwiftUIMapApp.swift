7a76641736bb618e3d9500d9e56ce869e2e0233c
Berat Yavuz <beratttyavuzz@gmail.com>
2022-06-08 14:17:52 +0300

    Initial Commit

---
+++ SwiftUIMap/SwiftUIMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapApp.swift
+//  SwiftUIMap
+//
+//  Created by Berat Yavuz on 8.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
